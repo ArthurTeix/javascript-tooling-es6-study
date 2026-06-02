5bc49a8b489b4706e4eb90342ba2d49d11aab54b
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-02 07:11:02 -0300

    docs: add comands babel install and to use

---
+++ babel/notes.js
@@ -0,0 +1,7 @@
+/*
+    1- Comando de instalação babel e suas dependências iniciais
+npm install --save-dev @babel/cli @babel/core @babel/preset-env
+
+    2- Comando para converter o main.js em bundle.js
+./node_modules/.bin/babel babel/main.js -o babel/bundle.js --presets=@babel/env
+*/
