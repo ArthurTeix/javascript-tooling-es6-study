4f89bb069fb021d8c96126450fdae9413c07b4c0
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-01 22:49:13 -0300

    docs: add notes about babel

--- babel/bundle.js
+++ babel/bundle.js
@@ -1,3 +1,5 @@
+// Este arquivo é o mesmo script que o do main.js, mas refatorado para rodar em todo navegador web
+
 "use strict";
 
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
--- babel/main.js
+++ babel/main.js
@@ -1,3 +1,5 @@
+// usando o babel, todo código escrito nesse arquivo será reescrito num novo arquivo bundle.js, onde lá o código será compatível com todos os navegadores possíveis
+
 class Pessoa {
     constructor(nome, sobrenome) {
         this.nome = nome,
