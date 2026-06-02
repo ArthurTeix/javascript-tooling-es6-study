3e93da060ba5fe2fb0d9d56a857c102feb41fba5
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-02 08:23:59 -0300

    feat: add script 'gerador' to webpack translator auto

---
+++ webpack(boilerplate)/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// Essa pasta está sendo assistida pelo meu script 'gearador', toda alteração aqui será refletida no bundle.js dentro da pasta public (que eu não mexo lá)
+console.log('Olá, webpack!');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ webpack(boilerplate)/src/index.js
@@ -0,0 +1,2 @@
+// Essa pasta está sendo assistida pelo meu script 'gearador', toda alteração aqui será refletida no bundle.js dentro da pasta public (que eu não mexo lá)
+console.log('Olá, webpack!')
