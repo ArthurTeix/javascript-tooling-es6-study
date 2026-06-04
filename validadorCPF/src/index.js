6ba0a01a744df34c281b07e634de11fbcb942c32
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-04 19:02:01 -0300

    initial setup validator and generate CPF

---
+++ validadorCPF/src/index.js
@@ -0,0 +1,5 @@
+// Já que o GeraCPF também importa o ValidaCPF, então um único import acaba englobando dois arquivos (GeraCPF e ValidaCPF)
+import GeraCPF from './modules/GeraCPF.js'
+
+import './assets/css/style.css'
+
---
+++ validadorCPF/src/modules/GeraCPF.js
@@ -0,0 +1,9 @@
+import ValidaCPF from "./ValidaCPF"
+
+export default class GeraCPF {
+    rand(min = 100000000, max = 999999999) {
+        return String(Math.floor(Math.random() * (max - min) + min))
+    }
+
+    
+}
