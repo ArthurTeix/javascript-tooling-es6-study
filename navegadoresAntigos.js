a80401a482fa51c077a09875ebbb7534f7dbf654
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-08 08:28:30 -0300

    docs: add notes old browsers

---
+++ navegadoresAntigos.js
@@ -0,0 +1,8 @@
+/*
+Sempre que quiser compatibilidade com navegadores mais antigos, devo usar:
+
+-> import 'core-js/stable'
+-> import 'regenerator-runtime/runtime'
+
+Me permite usar de recurso mais modernos do js, como Promises e suas funcionalidades
+*/
