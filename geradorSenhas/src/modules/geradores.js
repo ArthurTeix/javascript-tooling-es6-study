94ed19b59cd1d258d48425dea5732ba511d90760
Arthur Teixeira <126914319+ArthurTeix@users.noreply.github.com>
2026-06-07 21:37:55 -0300

    feat: add function password generator

--- geradorSenhas/src/modules/geradores.js
+++ geradorSenhas/src/modules/geradores.js
@@ -3,17 +3,30 @@ const random = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
+// Códigos de números vão do 48 ao 57
+const geraNumeros = () => String.fromCharCode(random(48, 58))
+
 // Códigos de letras maiúsculas vão do 65 ao 90
 const geraMaiuscula = () => String.fromCharCode(random(65, 91))
 
 // Códigos de letras minúsculas vão do 97 ao 122
 const geraMinuscula = () => String.fromCharCode(random(97, 123))
 
-// Códigos de números vão do 48 ao 57
-const geraNumeros = () => String.fromCharCode(random(48, 58))
-
 // Códigos de símbolos são misturados, então vou gerar manualmente
 const simbolos = "!@#$%¨&*()_-+=`[{ª~^]}º,.<>;:/?°|§"
 const geraSimbolos = () => simbolos[random(0, simbolos.length)]
 
-console.log(geraSimbolos())
+// Função Geradora
+function geraSenha(quant, num, maiusc, minusc, simb) {
+    const senha = []
+    quant = Number(quant)
+
+    for (let i = 0; i < quant; i++) {
+        num && senha.push(geraNumeros())
+        maiusc && senha.push(geraMaiuscula())
+        minusc && senha.push(geraMinuscula())
+        simb && senha.push(geraSimbolos())
+    }
+
+    return senha.join('').slice(0, quant)
+}
